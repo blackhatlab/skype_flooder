2d7e740365b47acf01bab357e46417db53a5edf0
BlackHat Lab <back.hat.lab@gmail.com>
2016-01-01 01:57:02 +0300

    src

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ print ' '*10 + 'Working Directory: ' + path + ' '*10
 print '='*70
 
 try:
+   os.system ('sudo pip install colorama && sudo pip install Skype4Py')
    os.system ('sudo mv "' + script + '/src/skype-flooder" /usr/bin')
    os.system ('sudo chmod +x /usr/bin/skype-flooder')
    os.system ('sudo rmdir "' + script + '/src"')
